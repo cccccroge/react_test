399f0bec653b9e8318ae587699010c7accc68897
cccccroge <pooopooo543@gmail.com>
2020-08-10 01:19:08 +0800

    Change data_storage format

--- src/todoList/model/ToDoListContext.js
+++ src/todoList/model/ToDoListContext.js
@@ -3,24 +3,28 @@ import { createContext } from "react";
 /* emulate data storage from server */
 export const data_storage = {
   categories: ["daily", "art", "programming"],
+  current_category: "",
   tasks: [
     {
       name: "eat",
       description: "I should eat",
-      deadline: null,
+      deadline: new Date(2020, 8, 15),
       category: "daily",
+      priority: "medium",
     },
     {
       name: "shit",
       description: "I should shit",
-      deadline: null,
+      deadline: new Date(2020, 8, 20),
       category: "daily",
+      priority: "low",
     },
     {
       name: "cao",
       description: "I should cao",
-      deadline: null,
+      deadline: new Date(2020, 9, 13),
       category: "daily",
+      priority: "high",
     },
   ],
 };
