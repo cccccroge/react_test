d1985310d15d1ab6aa8d26d0cc0d1969743417db
cccccroge <pooopooo543@gmail.com>
2020-08-09 17:33:43 +0800

    Add edit mode to CategoryBtn, change theme, add issue.md

--- src/todoList/component/CategoryBtn.jsx
+++ src/todoList/component/CategoryBtn.jsx
@@ -1,17 +1,55 @@
 import "../stylesheet/CategoryBtn.scss";
-import React from "react";
+import React, { useState, useRef } from "react";
 
 const CategoryBtn = (props) => {
   const { name } = props;
 
+  const [ isEdit, setIsEdit ] = useState(false);
+  let editName = name;
+
+  const inputRef = useRef(null);
+
+  // Event
+  const handleBtnDoubleClick = (e) => {
+    e.preventDefault();
+    setIsEdit(true);
+    setTimeout(() => {
+      const input = inputRef.current;
+      focusToTextEnd(input);
+    }, 0);
+  };
+
+  const focusToTextEnd = (element) => {
+    var range = document.createRange();
+    var sel = window.getSelection();
+    range.setStart(element, 1);
+    sel.removeAllRanges();
+    sel.addRange(range);
+
+    element.focus();
+  };
+
+  // Render
   return (
     <button
       className="category-btn"
       type="button"
       name="category"
       value={name}
+      onDoubleClick={handleBtnDoubleClick}
     >
-      {name}
+      {isEdit && (
+        <span
+          ref={inputRef}
+          className="category-input"
+          role="textbox"
+          contentEditable
+          suppressContentEditableWarning="true"
+        >
+          {editName}
+        </span>
+      )}
+      {!isEdit && name}
     </button>
   );
 };
---
+++ src/todoList/issues.md
@@ -0,0 +1,7 @@
+## Known Issues about TODO list application
+
+## UI
+- flex container of CategoryBtns will grow right while adding longer content in any CategoryBtn
+- scrollbar style of CategoryList is supported only for Firefox
+- the '+' character in AddBtn is slightly off center
+- edit mode of CategoryBtn behavior: when empty, cursor is slightly off center vertically
